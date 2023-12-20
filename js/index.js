d2e257bd1d63535ef72389dab8b39c4d24f1994f
tictools <jaumevillarreal@gmail.com>
2023-12-20 10:51:05 +0100

    feat: disable button when promise is fulfilled

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 import { fetchPokemons } from "./fetchPokemons.js";
 
 const $button = globalThis.document.querySelector(".main__button");
-const $message = globalThis.document.querySelector(".main__message");
-const $list = globalThis.document.querySelector(".main__list");
 
 $button.addEventListener("click", function () {
+  const $message = globalThis.document.querySelector(".main__message");
+  const $list = globalThis.document.querySelector(".main__list");
+
   $list.classList.remove("hidden");
 
   fetchPokemons().then((parsedData) => {
     const { pokemons, total } = parsedData; // object destructuring
 
     $message.textContent = `Total: ${total} pokemons`;
+    $button.disabled = true;
 
     pokemons.map((pokemon) => {
       const $itemList = globalThis.document.createElement("li");
